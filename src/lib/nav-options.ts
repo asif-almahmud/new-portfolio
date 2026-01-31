700b1bb12fe89cafe1f512c59f52354d1ba3afbc
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2026-01-31 10:37:59 +0600

    feat: Add "experience" navigation option to the navigation options list.

--- src/lib/nav-options.ts
+++ src/lib/nav-options.ts
@@ -1,6 +1,7 @@
 export const navOptions = [
   { option: "home", icon: "uil-estate" },
   { option: "about", icon: "uil-user" },
+  { option: "experience", icon: "uil-briefcase" },
   { option: "skills", icon: "uil-file-alt" },
   { option: "portfolio", icon: "uil-scenery" },
   { option: "contact", icon: "uil-message" },
